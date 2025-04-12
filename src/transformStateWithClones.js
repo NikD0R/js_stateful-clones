bc202e02d040d9fcfe9f743e996a693068483cf5
Nikita Doroshenko <nikitokd05@gmail.com>
2025-04-12 22:44:28 +0300

    solution with switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,27 +11,29 @@ function transformStateWithClones(state, actions) {
   const stateNew = { ...state };
 
   for (const n of actions) {
-    if (n.type === 'addProperties') {
-      for (const val of Object.entries(n.extraData)) {
-        stateNew[val[0]] = val[1];
-      }
-      actionsCopy.push({ ...stateNew });
-    }
-
-    if (n.type === 'removeProperties') {
-      for (const val of n.keysToRemove) {
-        if (Object.hasOwn(stateNew, val)) {
-          delete stateNew[val];
+    switch (n.type) {
+      case 'addProperties':
+        for (const val of Object.entries(n.extraData)) {
+          stateNew[val[0]] = val[1];
         }
-      }
-      actionsCopy.push({ ...stateNew });
-    }
+        actionsCopy.push({ ...stateNew });
+        break;
 
-    if (n.type === 'clear') {
-      for (const entry of Object.entries(stateNew)) {
-        delete stateNew[entry[0]];
-      }
-      actionsCopy.push({ ...stateNew });
+      case 'removeProperties':
+        for (const val of n.keysToRemove) {
+          if (Object.hasOwn(stateNew, val)) {
+            delete stateNew[val];
+          }
+        }
+        actionsCopy.push({ ...stateNew });
+        break;
+
+      case 'clear':
+        for (const entry of Object.entries(stateNew)) {
+          delete stateNew[entry[0]];
+        }
+        actionsCopy.push({ ...stateNew });
+        break;
     }
   }
 
